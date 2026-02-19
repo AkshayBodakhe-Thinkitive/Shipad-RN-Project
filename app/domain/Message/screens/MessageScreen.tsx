1860221d047b3ce500d9c7c1d831310d16dc4abb
Shripad1103 <155047531+Shripad1103@users.noreply.github.com>
2026-02-19 18:34:21 +0530

    modifications in message screen

--- app/domain/Message/screens/MessageScreen.tsx
+++ app/domain/Message/screens/MessageScreen.tsx
@@ -6,6 +6,8 @@ import {
   TouchableOpacity,
   FlatList,
   StyleSheet,
+  KeyboardAvoidingView,
+  Platform,
 } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { io, Socket } from 'socket.io-client';
@@ -13,17 +15,20 @@ import { io, Socket } from 'socket.io-client';
 type Message = {
   sender: string;
   text: string;
+  time: string;
+  status?: 'sent' | 'seen';
 };
 
 const ChatScreen = () => {
   const socketRef = useRef<Socket | null>(null);
+  const flatListRef = useRef<FlatList>(null);
 
   const [message, setMessage] = useState<string>('');
   const [messages, setMessages] = useState<Message[]>([]);
   const [onlineUsers, setOnlineUsers] = useState<string[]>([]);
 
-  const senderId = 'user2';
-  const receiverId = 'user1';
+  const senderId = 'Shripad';
+  const receiverId = 'Atif';
 
   const isReceiverOnline = onlineUsers.includes(receiverId);
 
@@ -35,10 +40,16 @@ const ChatScreen = () => {
     socketRef.current.emit('register', senderId);
 
     socketRef.current.on('receive_message', (data: any) => {
-      setMessages(prev => [
-        ...prev,
-        { sender: data.senderId, text: data.message },
-      ]);
+      const newMsg: Message = {
+        sender: data.senderId,
+        text: data.message,
+        time: new Date().toLocaleTimeString([], {
+          hour: '2-digit',
+          minute: '2-digit',
+        }),
+      };
+
+      setMessages(prev => [...prev, newMsg]);
     });
 
     socketRef.current.on('online_users', (users: string[]) => {
@@ -50,8 +61,25 @@ const ChatScreen = () => {
     };
   }, []);
 
+  
+    useEffect(() => {
+      if (messages.length > 0) {
+        flatListRef.current?.scrollToEnd({ animated: true });
+      }
+    }, [messages]);
+
   const sendMessage = () => {
     if (!message.trim()) return;
+
+    const newMsg: Message = {
+      sender: senderId,
+      text: message,
+      time: new Date().toLocaleTimeString([], {
+        hour: '2-digit',
+        minute: '2-digit',
+      }),
+      status: isReceiverOnline ? 'seen' : 'sent',
+    };
 
     socketRef.current?.emit('send_message', {
       senderId,
@@ -59,70 +87,97 @@ const ChatScreen = () => {
       message,
     });
 
-    setMessages(prev => [...prev, { sender: senderId, text: message }]);
-
+    setMessages(prev => [...prev, newMsg]);
     setMessage('');
   };
 
   return (
     <SafeAreaView style={styles.safeArea}>
-      <View style={styles.container}>
-        
-        <View style={styles.header}>
-          <Text style={styles.username}>{receiverId}</Text>
+      <KeyboardAvoidingView
+        style={{ flex: 1 }}
+        behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+      >
+        <View style={styles.container}>
+          {/* HEADER */}
+          <View style={styles.header}>
+            <Text style={styles.username}>{receiverId}</Text>
 
-          <View style={styles.statusContainer}>
-            <View
-              style={[
-                styles.statusDot,
-                { backgroundColor: isReceiverOnline ? '#4CAF50' : '#777' },
-              ]}
-            />
-            <Text style={styles.statusText}>
-              {isReceiverOnline ? 'Online' : 'Offline'}
-            </Text>
-          </View>
-        </View>
-
-        <FlatList
-          data={messages}
-          keyExtractor={(_, index) => index.toString()}
-          contentContainerStyle={{ paddingVertical: 10 }}
-          renderItem={({ item }) => (
-            <View
-              style={[
-                styles.messageBubble,
-                item.sender === senderId
-                  ? styles.myMessage
-                  : styles.otherMessage,
-              ]}
-            >
-              <Text
+            <View style={styles.statusContainer}>
+              <View
                 style={[
-                  styles.messageText,
-                  item.sender === senderId && { color: '#fff' },
+                  styles.statusDot,
+                  { backgroundColor: isReceiverOnline ? '#4CAF50' : '#777' },
                 ]}
-              >
-                {item.text}
+              />
+              <Text style={styles.statusText}>
+                {isReceiverOnline ? 'Online' : 'Offline'}
               </Text>
             </View>
-          )}
-        />
+          </View>
 
-        <View style={styles.inputWrapper}>
-          <TextInput
-            style={styles.input}
-            value={message}
-            onChangeText={setMessage}
-            placeholder="Type a message..."
-            placeholderTextColor="#888"
+          {/* CHAT LIST */}
+          <FlatList
+            ref={flatListRef}
+            data={messages}
+            keyExtractor={(_, index) => index.toString()}
+            contentContainerStyle={{
+              flexGrow: 1,
+              justifyContent: 'flex-end',
+              paddingVertical: 10,
+            }}
+            renderItem={({ item }) => (
+              <View
+                style={[
+                  styles.messageBubble,
+                  item.sender === senderId
+                    ? styles.myMessage
+                    : styles.otherMessage,
+                ]}
+              >
+                <Text
+                  style={[
+                    styles.messageText,
+                    item.sender === senderId && { color: '#fff' },
+                  ]}
+                >
+                  {item.text}
+                </Text>
+
+                <View style={styles.metaRow}>
+                  <Text style={styles.timeText}>{item.time}</Text>
+
+                  {item.sender === senderId && (
+                    <Text
+                      style={[
+                        styles.tick,
+                        {
+                          color: item.status === 'seen' ? '#ffffff' : '#ffffff',
+                        },
+                      ]}
+                    >
+                      ✓
+                    </Text>
+                  )}
+                </View>
+              </View>
+            )}
           />
-          <TouchableOpacity style={styles.sendButton} onPress={sendMessage}>
-            <Text style={styles.sendText}>Send</Text>
-          </TouchableOpacity>
-        </View>
 
-      </View>
+          {/* INPUT */}
+          <View style={styles.inputWrapper}>
+            <TextInput
+              style={styles.input}
+              value={message}
+              onChangeText={setMessage}
+              placeholder="Type a message..."
+              placeholderTextColor="#777"
+            />
+            <TouchableOpacity style={styles.sendButton} onPress={sendMessage}>
+              <Text style={styles.sendText}>Send</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </KeyboardAvoidingView>
     </SafeAreaView>
   );
 };
@@ -143,14 +198,14 @@ const styles = StyleSheet.create({
   header: {
     paddingVertical: 15,
     borderBottomWidth: 1,
-    borderBottomColor: '#222',
-    alignItems:'center',    
+    borderBottomColor: '#0a0a0a',
+    alignItems: 'center',
   },
 
   username: {
     fontSize: 20,
     fontWeight: 'bold',
-    color: '#121111',
+    color: '#292525',
   },
 
   statusContainer: {
@@ -167,58 +222,74 @@ const styles = StyleSheet.create({
   },
 
   statusText: {
-    color: '#221e1e',
+    color: '#181616',
     fontSize: 13,
   },
-
 
   messageBubble: {
     padding: 12,
     marginVertical: 6,
-    borderRadius: 15,
+    borderRadius: 16,
     maxWidth: '75%',
   },
 
   myMessage: {
-    backgroundColor: '#1E88E5',
+    backgroundColor: '#2563EB',
     alignSelf: 'flex-end',
-    borderTopRightRadius: 0,
+    borderTopRightRadius: 4,
   },
 
   otherMessage: {
-    backgroundColor: '#41596e',
+    backgroundColor: '#1F2937',
     alignSelf: 'flex-start',
-    borderTopLeftRadius: 0,
+    borderTopLeftRadius: 4,
   },
 
   messageText: {
-    color: '#ddd',
-    fontSize: 15,
+    color: '#E5E7EB',
+    fontSize: 18,
   },
 
+  metaRow: {
+    flexDirection: 'row',
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+    marginTop: 4,
+  },
+
+  timeText: {
+    fontSize: 15,
+    color: '#fff',
+    marginRight: 4,
+  },
+
+  tick: {
+    fontSize: 11,
+  },
 
   inputWrapper: {
     flexDirection: 'row',
     alignItems: 'center',
     paddingVertical: 10,
     borderTopWidth: 1,
-    borderTopColor: '#222',
+    borderTopColor: '#1F1F1F',
   },
 
   input: {
     flex: 1,
-    backgroundColor: '#1E1E1E',
+    backgroundColor: '#fff',
     borderRadius: 10,
     paddingHorizontal: 15,
     paddingVertical: 15,
-    color: '#fff',
+    color: '#1b1919',
     marginRight: 10,
+    borderWidth: 1,
   },
 
   sendButton: {
-    backgroundColor: '#1E88E5',
+    backgroundColor: '#2563EB',
     paddingVertical: 15,
-    paddingHorizontal: 18,
+    paddingHorizontal: 20,
     borderRadius: 10,
   },
 
--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 /**
  * @format
  */
-
 import { AppRegistry } from 'react-native';
 import App from './App';
 import { name as appName } from './app.json';
 import { Provider } from 'react-redux';
-// import { store } from './app/store/StoreConfigs';
 import { PersistGate } from 'redux-persist/integration/react';
 import { store, persistor } from './app/store/storeConfig';
 
